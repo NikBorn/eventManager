99176e8046709eb62ca2da9a27813b71d11214d3
Nik Bornemeier <NikolausRB@gmail.com>
2018-05-03 15:22:31 -0600

    remove duplicate declaration

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -2,8 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { FormioAuth, FormioAuthRoutes } from 'angular-formio/auth';
-import { RouterModule } from '@angular/router';
-import { FormioAuth, FormioAuthRoutes } from 'angular-formio/auth';
+
 
 @NgModule({
   imports: [
