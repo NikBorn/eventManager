cc30100bf189d304db3a70bb98dca0b11f567f90
Nik Bornemeier <NikolausRB@gmail.com>
2018-05-08 14:53:14 -0600

    add event module

--- src/app/event/event.module.ts
+++ src/app/event/event.module.ts
@@ -2,12 +2,20 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 import { FormioResource, FormioResourceRoutes, FormioResourceConfig, FormioResourceService } from 'angular-formio/resource';
+import { ParticipantModule } from './participant/participant.module';
+
+const eventResourceRoutes: Routes = FormioResourceRoutes({});
+
+eventResourceRoutes[2].children.push({
+  path: 'participant',
+  loadChildren: () => ParticipantModule
+});
 
 @NgModule({
   imports: [
     CommonModule,
     FormioResource,
-    RouterModule.forChild(FormioResourceRoutes())
+    RouterModule.forChild(eventResourceRoutes)
   ],
   declarations: [],
   providers: [
